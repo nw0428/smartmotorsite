790b2f59b3788e325085ba96df701a0791bcdf40
David Zabner <david.zabner@gmail.com>
2022-03-16 23:36:10 -0400

    can now handle motor registrations

---
+++ handle-registration.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -f "motor_list.txt" ]; then
+    export MOTORS=`cat motor_list.txt`
+else
+    export MOTORS="[]"
+fi
+
+mosquitto_sub -t motors/register | while read -r line;
+    do export MOTORS=$(echo $line | jq -c '[.] + (env.MOTORS|fromjson)')
+    echo $MOTORS > motor_list.txt
+    mosquitto_pub -t motors/list -r -m "$MOTORS"
+done
